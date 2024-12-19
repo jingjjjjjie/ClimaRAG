b465e68d80fdc08689fc53b78308cd44379d258e
Xiangyu Wong <wongxiangyu22@gmail.com>
2024-12-20 01:50:54 +0800

    Updated required packages

--- src/setup.py
+++ src/setup.py
@@ -22,6 +22,7 @@ setup(
         "requests",
         "google-api-python-client",
         "typing-extensions",
+        "lark",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
