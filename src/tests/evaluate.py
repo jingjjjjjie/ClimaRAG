aa5e4a0516b6dc4b9d4f9d09367f7e18c713603e
Xiangyu Wong <wongxiangyu22@gmail.com>
2025-01-08 00:06:23 +0800

    Changed queries

--- src/tests/evaluate.py
+++ src/tests/evaluate.py
@@ -3,8 +3,7 @@ import requests
 response = requests.post(
     "http://localhost:8000/api/v1/evaluate",
     json={"text": "What are the impacts of climate change on food?"}
-    # json={"text": "Tell me about the transformer architecture in detail"}
-    # json={"text": "What did we talk about transformer architecture just now?"}
-    # json={"text": "summarize advancements in the field natural language processing on the year 2020"}
+    # json={"text": "Summarize advancements in the field of climate change on the year 2020"}
+    # json={"text": "What did we talk about climate change just now?"}
 )
 print(response.json())
--- src/tests/query.py
+++ src/tests/query.py
@@ -2,8 +2,7 @@ import requests
 
 response = requests.post(
     "http://localhost:8000/api/v1/query",
-    # json={"text": "What are the impacts of climate change on food?"}
-    json={"text": "Tell me about the climate change in detail"}
+    json={"text": "What are the impacts of climate change on food?"}
     # json={"text": "Summarize advancements in the field of climate change on the year 2020"}
     # json={"text": "What did we talk about climate change just now?"}
 )
