6657e6ed7697b005a60e0375e3107e9e9a48c6b4
Xiangyu Wong <wongxiangyu22@gmail.com>
2025-01-07 23:31:24 +0800

    Changed queries

--- src/tests/query.py
+++ src/tests/query.py
@@ -2,9 +2,9 @@ import requests
 
 response = requests.post(
     "http://localhost:8000/api/v1/query",
-    json={"text": "What are the impacts of climate change on food?"}
-    # json={"text": "Tell me about the transformer architecture in detail"}
-    # json={"text": "What did we talk about transformer architecture just now?"}
-    # json={"text": "summarize advancements in the field natural language processing on the year 2020"}
+    # json={"text": "What are the impacts of climate change on food?"}
+    json={"text": "Tell me about the climate change in detail"}
+    # json={"text": "Summarize advancements in the field of climate change on the year 2020"}
+    # json={"text": "What did we talk about climate change just now?"}
 )
 print(response.json())
