0202ad33463c469ca950d85176215d292b9cc2f4
jhchong0405 <jhchong14021@gmail.com>
2025-01-11 08:54:24 +0800

    Added memory limit

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,6 +12,11 @@ LLM_MODEL = "gpt-4o"
 CHUNK_SIZE = 1500
 CHUNK_OVERLAP = 100
 
+# Memory limit: the number of the last k messages to remember
+# This is used to limit the number of messages to remember in the memory
+# This includes the user messages and the replies from the AI
+MEMORY_LIMIT = 5
+
 # Data Settings
 # DATA_PATH = './src/data/data_info.txt'
 DATA_PATH = './src/data/output4.json'
